b7a001761fa715d460b11ca9190b30bc3b4d3cc5
Viradj Jainandunsing <viradj@brandcube.nl>
2018-05-25 13:03:22 +0200

    fix

--- JavaGPIO/src/MainClass.java
+++ JavaGPIO/src/MainClass.java
@@ -6,17 +6,25 @@ import com.pi4j.io.gpio.RaspiPin;
 public class MainClass {
 
     public static void main(String[] args) {
-        GpioController gpioController = GpioFactory.getInstance();
-        GpioPinDigitalOutput pinOut = gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_01);
-
-        pinOut.high();
 
         try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
+            GpioController gpioController = GpioFactory.getInstance();
+            GpioPinDigitalOutput pinOut = gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_01);
+
+            pinOut.high();
+
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            pinOut.low();
+        } catch  (UnsatisfiedLinkError e){
+            System.err.println("platform does not support this driver");
+        } catch (Exception e) {
+            System.err.println("Een andere error message");
             e.printStackTrace();
         }
-
-        pinOut.low();
     }
 }
