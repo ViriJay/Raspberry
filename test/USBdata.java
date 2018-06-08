2a0be7e42531b48275617d379fef80d34bde63c0
Viradj Jainandunsing <viradj@brandcube.nl>
2018-06-08 14:36:05 +0200

    USBdata.java

---
+++ test/USBdata.java
@@ -0,0 +1,114 @@
+// START SNIPPET: serial-snippet
+
+
+/*
+ * #%L
+ * **********************************************************************
+ * ORGANIZATION  :  Pi4J
+ * PROJECT       :  Pi4J :: Java Examples
+ * FILENAME      :  SerialExample.java
+ *
+ * This file is part of the Pi4J project. More information about
+ * this project can be found here:  http://www.pi4j.com/
+ * **********************************************************************
+ * %%
+ * Copyright (C) 2012 - 2018 Pi4J
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
+
+import com.pi4j.io.serial.*;
+
+import java.io.IOException;
+import java.util.Date;
+
+/**
+ * This example code demonstrates how to perform serial communications using the Raspberry Pi.
+ *
+ * @author Robert Savage
+ */
+public class USBdata {
+
+    /**
+     * This example program supports the following optional command arguments/options:
+     *   "--device (device-path)"                   [DEFAULT: /dev/ttyAMA0]
+     *   "--baud (baud-rate)"                       [DEFAULT: 38400]
+     *   "--data-bits (5|6|7|8)"                    [DEFAULT: 8]
+     *   "--parity (none|odd|even)"                 [DEFAULT: none]
+     *   "--stop-bits (1|2)"                        [DEFAULT: 1]
+     *   "--flow-control (none|hardware|software)"  [DEFAULT: none]
+     *
+     * @param args
+     * @throws InterruptedException
+     * @throws IOException
+     */
+    public static void main(String args[]) throws InterruptedException, IOException {
+
+        // !! ATTENTION !!
+        // By default, the serial port is configured as a console port
+        // for interacting with the Linux OS shell.  If you want to use
+        // the serial port in a software program, you must disable the
+        // OS from using this port.
+        //
+        // Please see this blog article for instructions on how to disable
+        // the OS console for this port:
+        // https://www.cube-controls.com/2015/11/02/disable-serial-port-terminal-output-on-raspbian/
+
+        // create an instance of the serial communications class
+        final Serial serial = SerialFactory.createInstance();
+
+        try {
+            // create serial config object
+            SerialConfig config = new SerialConfig();
+
+            // set default serial settings (device, baud rate, flow control, etc)
+            //
+            // by default, use the DEFAULT com port on the Raspberry Pi (exposed on GPIO header)
+            // NOTE: this utility method will determine the default serial port for the
+            //       detected platform and board/model.  For all Raspberry Pi models
+            //       except the 3B, it will return "/dev/ttyAMA0".  For Raspberry Pi
+            //       model 3B may return "/dev/ttyS0" or "/dev/ttyAMA0" depending on
+            //       environment configuration.
+            config.device("/dev/ttyACM0")
+                  .baud(Baud._9600)
+                  .dataBits(DataBits._8)
+                  .parity(Parity.NONE)
+                  .stopBits(StopBits._1)
+                  .flowControl(FlowControl.NONE);
+
+            // parse optional command argument options to override the default serial settings.
+
+            // open the default serial device/port with the configuration settings
+            serial.open(config);
+
+            // continuous loop to keep the program running until the user terminates the program
+            while(true) {
+                System.out.println(serial.getInputStream());
+
+                // wait 1 second before continuing
+                Thread.sleep(1000);
+            }
+
+        }
+        catch(IOException ex) {
+            console.println(" ==>> SERIAL SETUP FAILED : " + ex.getMessage());
+            return;
+        }
+    }
+}
+
+// END SNIPPET: serial-snippet
